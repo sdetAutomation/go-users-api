8939e7486e558f1ed08cd4af4997eac82e33c297
sdetAutomation <hkw@hkws-MBP.lan>
2020-04-15 23:40:53 -0500

    chore: add funtions placeholders for future use

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,2 +1,21 @@
 package controllers
 
+// CreateUser ...
+func CreateUser() {
+
+}
+
+// GetUser ...
+func GetUser() {
+
+}
+
+// UpdateUser ...
+func UpdateUser() {
+
+}
+
+// DeleteUser ...
+func DeleteUser() {
+
+}

aab600890702876474bddaa16808a9d108ad9aa1
sdetAutomation <hkw@hkws-MacBook-Pro.local>
2020-04-20 00:59:43 -0500

    refactor: update sqlite utils function table schema and update local.db file

--- utils/sqlite/sqlite_utils.go
+++ utils/sqlite/sqlite_utils.go
@@ -10,11 +10,11 @@ import (
 // CreateDb ...
 func CreateDb() {
     database, _ := sql.Open("sqlite3", "./sqlite/local.db")
-    statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, email TEXT, datecreated TEXT)")
+    statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, first_name TEXT, last_name TEXT, email TEXT NOT NULL UNIQUE, date_created TEXT)")
     statement.Exec()
-    statement, _ = database.Prepare("INSERT INTO users (id, firstname, lastname, email, datecreated) VALUES (?, ?, ?, ?, ?)")
-    statement.Exec(1, "sdet", "automation", "sdet.testautomation@gmail.com", "2020-04-19-T22:58:14Z")
-    rows, _ := database.Query("SELECT id, firstname, lastname, email, datecreated FROM users")
+    statement, _ = database.Prepare("INSERT INTO users (first_name, last_name, email, date_created) VALUES (?, ?, ?, ?)")
+    statement.Exec("sdet", "automation", "sdet.testautomation@gmail.com", "2020-04-19-T22:58:14Z")
+    rows, _ := database.Query("SELECT id, first_name, last_name, email, date_created FROM users")
     var id int
     var firstname string
     var lastname string
